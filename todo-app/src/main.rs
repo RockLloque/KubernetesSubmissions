f4dfbe904fda9cc5794af514a695ba99ed561791
Thomas Schmahl <thmschmahl@gmail.com>
2025-08-03 13:45:59 +0200

    feat: simple axum server

--- todo-app/src/main.rs
+++ todo-app/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::{Router, extract::State, routing::get};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let port = "3000".to_string();
+    let app = Router::new().route("/", get(root).with_state(port.clone()));
+
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await?;
+
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+async fn root(State(port): State<String>) -> String {
+    format!("Server started in port {}", port)
 }
