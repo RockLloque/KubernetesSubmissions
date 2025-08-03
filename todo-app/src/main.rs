b0d784a6b37380fcf2043c5a5b911cdbaed42ae7
Thomas Schmahl <thmschmahl@gmail.com>
2025-08-03 14:03:46 +0200

    feat: add dotenv library to load .env

--- todo-app/src/main.rs
+++ todo-app/src/main.rs
@@ -2,9 +2,11 @@ use std::env;
 
 use anyhow::Result;
 use axum::{Router, extract::State, routing::get};
+use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenv().ok();
     let port = env::var("PORT").unwrap_or("3000".to_string()).to_string();
     let app = Router::new().route("/", get(root).with_state(port.clone()));
 
