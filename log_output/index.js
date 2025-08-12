5c0025c2a088359af9d79208292bf23a6236761a
Thomas Schmahl <thmschmahl@gmail.com>
2025-08-12 17:32:54 +0200

    feat: implement express

--- log_output/index.js
+++ log_output/index.js
@@ -1,8 +1,23 @@
+import express from 'express';
+
+const app = express();
+
 const randomHash = crypto.randomUUID();
+const port = '3000';
+
+const randomHashFn = (hash) => {
+  return `${new Date().toISOString()}: ${hash}`;
+}
 
 
 const printRandomHash = (hash) => {
-  console.log(`${new Date().toISOString()}: ${hash}`);
+  console.log(randomHashFn(hash))
 }
-
 setInterval(printRandomHash, 5000, randomHash);
+
+
+app.get('/', (_req, res) => res.send(printRandomHash(randomHash)))
+
+app.listen(port, () => console.log(`Listening on port: ${port}`))
+
+
