27c45470d3e9e67123f5e7ccc01d1b221ade1d65
Thomas Schmahl <thmschmahl@gmail.com>
2025-08-22 19:09:48 +0200

    feat: add logging

--- log-output-split/reader/src/index.ts
+++ log-output-split/reader/src/index.ts
@@ -7,8 +7,9 @@ const PATH = "/usr/src/app/log/log.txt";
 
 app.get("/logs", (_req, res) => {
   const log = readFileSync(PATH, { encoding: 'utf8' });
+  console.log(log);
   return res.send(`<pre>${log}</pre>`);
 });
 
-app.listen(PORT, () => console.log(`Reader app is listening on port: ${PORT}`))
+app.listen(PORT, () => console.log(`Reader app is listening on port: ${PORT}`));
 
