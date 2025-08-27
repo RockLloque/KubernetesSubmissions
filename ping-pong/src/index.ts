c8ed0a0a4f552808217dba093218bc8a8e5b5022
Thomas Schmahl <thmschmahl@gmail.com>
2025-08-27 14:56:55 +0200

    feat: ping-pong write to file

--- ping-pong/src/index.ts
+++ ping-pong/src/index.ts
@@ -1,13 +1,22 @@
-
+import { writeFile } from "fs";
 import express from 'express';
 
 
+const PATH = "/tmp/kube/ping.txt";
 
 const app = express();
 const PORT = process.env.PORT ?? 3001;
 
 let count = 0;
 
-app.get("/pingpong", (_req, res) => `PingPong counter: ${++count}`);
-app.listen(PORT, () => console.log(`PingPong is listening on port: ${PORT}`))
+app.get("/pingpong", (_req, _res) => {
+  ++count;
+  writeFile(PATH, `${count}`, { encoding: 'utf8' }, err => {
+    if (err) {
+      console.log(`Error writing to ${PATH}: ${err}`)
+    }
+  });
+  return `PingPong counter: ${count}`;
+});
 
+app.listen(PORT, () => console.log(`PingPong is listening on port: ${PORT}`))
