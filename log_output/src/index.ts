3544fd23a965cca44ee21662e1758070d6215286
Thomas Schmahl <thmschmahl@gmail.com>
2025-09-07 16:37:38 +0200

    feat: fetch count from ping pong svc

--- log_output/src/index.ts
+++ log_output/src/index.ts
@@ -10,6 +10,7 @@ const randomHash = crypto.randomUUID();
 const PORT = process.env.PORT ?? '3000';
 const PING_PONG_PORT = process.env.PING_PONG_PORT ?? '3001';
 const PING_PONG_SERVICE_NAME = process.env.PING_PONG_SERVICE_NAME ?? 'ping-pong-svc';
+const PING_PONG_PATH = `http://${PING_PONG_SERVICE_NAME}:${PING_PONG_PORT}`;
 
 const randomHashFn = (hash: string) => {
   return `${new Date().toISOString()}: ${hash}`;
@@ -17,9 +18,23 @@ const randomHashFn = (hash: string) => {
 
 
 
-app.get('/', (_req, res) => {
-  const count = 0; // TODO: read from PingPong
-  return res.send(`${randomHashFn(randomHash)}\nPing / Pongs: ${count}`);
+app.get('/', async (_req, res) => {
+  try {
+    const resp = await fetch(PING_PONG_PATH);
+
+    if (!resp.ok) {
+      throw new Error(`Ping Pong service under ${PING_PONG_PATH} returned the error: ${resp.status} `);
+    }
+
+    const count = await resp.text() ?? 0;
+
+    return res.send(`${randomHashFn(randomHash)}\nPing / Pongs: ${count}`);
+  } catch (error: any) {
+    console.error(error.message);
+    return res
+      .status(500)
+      .send(`${randomHashFn(randomHash)}\nPing / Pongs: Error`);
+  }
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
