cd8ab25900f043d4cc150ab7f779ae52c4456693
Thomas Schmahl <thmschmahl@gmail.com>
2025-09-07 15:55:33 +0200

    cleanup: remove read from file code

--- log_output/src/index.ts
+++ log_output/src/index.ts
@@ -1,4 +1,3 @@
-import { readFileSync } from "fs";
 import crypto from "crypto";
 import express from 'express';
 import dotenv from 'dotenv';
@@ -11,22 +10,15 @@ const randomHash = crypto.randomUUID();
 const PORT = process.env.PORT ?? '3000';
 const PING_PONG_PORT = process.env.PING_PONG_PORT ?? '3001';
 const PING_PONG_SERVICE_NAME = process.env.PING_PONG_SERVICE_NAME ?? 'ping-pong-svc';
-const PATH = "/tmp/kube/ping.txt";
 
 const randomHashFn = (hash: string) => {
   return `${new Date().toISOString()}: ${hash}`;
 }
 
 
-const printRandomHash = (hash: string) => {
-  const count = readFileSync(PATH, { encoding: 'utf8' });
-  console.log(`${randomHashFn(hash)}\n Ping/ Pongs: ${count}`)
-}
-setInterval(printRandomHash, 5000, randomHash);
-
 
 app.get('/', (_req, res) => {
-  const count = readFileSync(PATH, { encoding: 'utf8' });
+  const count = 0; // TODO: read from PingPong
   return res.send(`${randomHashFn(randomHash)}\nPing / Pongs: ${count}`);
 })
 
