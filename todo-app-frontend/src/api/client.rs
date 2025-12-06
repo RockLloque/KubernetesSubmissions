4d233ae1efec3b6f73e8596f1c1ff3f16fccce9d
Thomas Schmahl <thmschmahl@gmail.com>
2025-12-06 21:33:51 +0100

    write client later

--- todo-app-frontend/src/api/client.rs
+++ todo-app-frontend/src/api/client.rs
@@ -1,8 +1,8 @@
-use reqwest::Client;
+// use reqwest::Client;
 
-pub fn api_client() -> Client {
-    Client::builder()
-        .timeout(std::time::Duration::from_secs(2))
-        .build()
-        .unwrap()
-}
+// pub fn api_client() -> Client {
+//     Client::builder()
+//         .timeout(std::time::Duration::from_secs(2))
+//         .build()
+//         .unwrap()
+// }
