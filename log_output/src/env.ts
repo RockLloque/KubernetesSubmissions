8d453efa009b7a781ea25c3b89d90d592b4a8705
Thomas Schmahl <thmschmahl@gmail.com>
2025-12-25 22:53:51 +0100

    feat: parse env

---
+++ log_output/src/env.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+
+
+const envSchema = z.object({
+  PORT: z.coerce.number().int().positive().default(3000),
+  PING_PONG_SERVICE_NAME: z.string().default("ping-pong-svc"),
+  PING_PONG_PORT: z.coerce.number().int().positive().default(3001),
+  PING_PONG_SUBDIRECTORY: z.string().default("pings"),
+  INFORMATION_FILE_PATH: z.string().default('/etc/information/information.txt')
+})
+
+
+export const env = envSchema.parse(process.env);
