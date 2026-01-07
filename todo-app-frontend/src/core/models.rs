60eecdb8f3e40cadfe1665f2dfc151150a26a3a6
Thomas Schmahl <thmschmahl@gmail.com>
2026-01-07 22:19:27 +0100

    fix: correct Todo struct

--- todo-app-frontend/src/core/models.rs
+++ todo-app-frontend/src/core/models.rs
@@ -1,9 +1,15 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
 pub struct Todo {
-    pub id: u32,
+    pub id: String,
     pub title: String,
     pub description: String,
     pub sorting_order: i32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CreateTodo {
+    pub title: String,
+    pub description: String,
 }
