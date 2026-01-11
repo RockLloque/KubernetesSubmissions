f4d8ae258adf0f2355002c74bdc2e6f0c3d14aaa
Thomas Schmahl <thmschmahl@gmail.com>
2026-01-11 18:09:02 +0100

    refactor: move manifests to top level

--- scrips/random-wiki/create_wiki_todo.sh
+++ scrips/random-wiki/create_wiki_todo.sh
@@ -9,12 +9,13 @@ echo "Random Wikipedia page: $WIKI_URL"
 PAGE_TITLE=$(echo "$WIKI_URL" | sed 's|.*/wiki/||' | sed 's/_/ /g')
 
 # Create the todo using the backend service within the cluster
-curl -X POST http://todo-app-backend-svc.project.svc.cluster.local:3001/todos \
+TODO_BACKEND_RESPONSE = $(curl -X POST http://$TODO_BACKEND_SERVICE_HOST:$PORT_BACKEND/todos \
   -H "Content-Type: application/json" \
   -d "{
     \"title\": \"$PAGE_TITLE\",
     \"description\": \"$WIKI_URL\"
-  }"
+  }")
 
 echo ""
 echo "Todo created for: $PAGE_TITLE"
+echo "POST response: $TODO_BACKEND_RESPONSE"
